486f2b3d42d2299f5300722bf5f5a82b246626ac
Rebecca Brown <31925939+rebecca8840@users.noreply.github.com>
2018-05-25 23:53:57 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -15,6 +15,6 @@ export default {
     return axios.post("/api/articles", articleData);
   },
   findArticles: function(topic, startYear, endYear) {
-    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=54d61f55f75244bb9819c29f5e41aa3f&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231");
+    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231");
   }
 };
